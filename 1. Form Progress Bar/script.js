d5f7852609506f4d24c19667801f3f8deb0b9798
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2022-10-08 06:44:55 +0100

    Iteration on Script

--- 1. Form Progress Bar/script.js
+++ 1. Form Progress Bar/script.js
@@ -1,4 +1,7 @@
-
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
 
 let currentActive = 1
 
