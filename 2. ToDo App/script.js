371f50742bd4be66caae62a9c464c49d04b04b21
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2022-10-09 19:29:48 +0100

    ToDo App - JS

--- 2. ToDo App/script.js
+++ 2. ToDo App/script.js
@@ -1,4 +1,6 @@
-
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todosUL = document.getElementById('todos');
 
 const todos = JSON.parse(localStorage.getItem('todos'))
 
