729ee5cb7440f597ca6094b7d12e532af368ad98
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2022-10-13 22:01:59 +0100

    Password Generator - JS

--- 5. Password Generator/script.js
+++ 5. Password Generator/script.js
@@ -6,3 +6,24 @@ const numbersEl = document.getElementById('numbers');
 const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
 const clipboardEl = document.getElementById('clipboard');
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+}
+
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea')
+    const password = resultEl.innerText
+
+    if(!password) { return }
+
+    textarea.value = password
+    document.body.appendChild(textarea)
+    textarea.select()
+    document.execCommand('copy')
+    textarea.remove()
+    alert('Password copied to clipboard!')
+})
