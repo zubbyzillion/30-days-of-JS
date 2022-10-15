fa8fc154a11759a18e4cdc577074351e16011670
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2022-10-15 22:13:53 +0100

    Clock - JS

---
+++ 7. Clock/script.js
@@ -0,0 +1,9 @@
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.second');
+const timeEl = document.querySelector('.time');
+const dateEl = document.querySelector('.date');
+const toggle = document.querySelector('.toggle');
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
