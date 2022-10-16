b500a6dffaa1ab35ac19d817dc007fccfcecc6b0
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2022-10-16 21:13:27 +0100

    TypeWriter Effect - JS

--- 8. TypeWriter Effect/script.js
+++ 8. TypeWriter Effect/script.js
@@ -3,3 +3,17 @@ const speedEl = document.getElementById('speed');
 const text = 'Hello, My name is Nzubechukwu P. Beluonwu-Ogbo';
 let idx = 1;
 let speed = 300 / speedEl.value;
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+
+    idx++;
+
+    if(idx > text.length) {
+        idx = 1
+    }
+
+    setTimeout(writeText, speed)
+}
