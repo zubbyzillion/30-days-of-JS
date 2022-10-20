8707b6858e5e520d29b01fa16b36d258746dcd2c
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2022-10-20 20:59:39 +0100

    Drawing App

--- 9. Drawing App/script.js
+++ 9. Drawing App/script.js
@@ -35,3 +35,19 @@ canvas.addEventListener('mousemove', (e) => {
         y = y2
     }
 })
+
+function drawCircle(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, Math.PI * 2)
+    ctx.fillStyle = color
+    ctx.fill()
+}
+
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath()
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.strokeStyle = color
+    ctx.lineWidth = size * 2
+    ctx.stroke()
+}
