aa77c2012e812128fda57b163258bc69308bda00
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2022-10-22 21:20:32 +0100

    Image Carousel - JS

--- 10. Image Carousel/script.js
+++ 10. Image Carousel/script.js
@@ -3,3 +3,21 @@ const leftBtn = document.getElementById('left');
 const rightBtn = document.getElementById('right');
 
 const img = document.querySelectorAll('#imgs img');
+let idx = 0
+
+let interval = setInterval(run, 2000)
+
+function run() {
+    idx++
+    changeImage()
+}
+
+function changeImage() {
+    if(idx > img.length) {
+        idx = 0
+    } else if(idx < 0) {
+        idx = img.length - 1
+    }
+
+    imgs.style.transform = `translateX(${-idx * 500}px)`
+}
