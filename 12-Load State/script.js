52e05dc54a95ac527418abd590947b14b8c5d67e
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-07 23:57:31 +0100

    Added timeOut Function

--- 12-Load State/script.js
+++ 12-Load State/script.js
@@ -5,20 +5,25 @@ const cardDescription = document.querySelector('.card_description');
 const renderCard = () => {
     cardTitle.textContent = 'Here goes the Card Title';
     cardDescription.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quia suscipit placeat quod pariatur. Aliquid iste totam sed inventore veniam! Perspiciatis et voluptate modi culpa qui, vero autem obcaecati fugiat?';
-
-    cardTitle.classList.remove('.loading');
-    cardDescription.classList.remove('.loading');
-    cardImage.classList.remove('.loading');
+    createImage();
+    cardTitle.classList.remove('loading');
+    cardDescription.classList.remove('loading');
+    cardImage.classList.remove('loading');
 };
 
 function createImage() {
     const img = new Image();
     img.classList.add("image");
     img.setAttribute('alt', 'A randomly generated image');
-    img.setAttribute('src', 'https://source.unsplash.com/random/320x200');
+    img.setAttribute('src', 'https://source.unsplash.com/random/380x200');
     cardImage.appendChild(img);
-
-    console.log(img);
 };
 
-createImage();
+// TimeOut function to imitate getting images from server
+setTimeout(() => {
+    renderCard();
+}, 5000);
+
+// window.addEventListener('load', () => {
+//     renderCard();
+// })
