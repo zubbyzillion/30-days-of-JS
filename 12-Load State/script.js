918cadc43ac0dec28f8cde2f8cc5af4d3a6bbd9b
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-07 23:28:22 +0100

    Adding JS - 2

--- 12-Load State/script.js
+++ 12-Load State/script.js
@@ -6,4 +6,16 @@ console.log(CardTitle);
 const renderCard = () => {
     cardTitle.textContent = 'Here goes the Card Title';
     cardDescription.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quia suscipit placeat quod pariatur. Aliquid iste totam sed inventore veniam! Perspiciatis et voluptate modi culpa qui, vero autem obcaecati fugiat?';
+
+    cardTitle.classList.remove('.loading');
+    cardDescription.classList.remove('.loading');
+    cardImage.classList.remove('.loading');
+};
+
+function createImage() {
+    const img = new Image();
+
+    console.log(img);
 }
+
+createImage();
