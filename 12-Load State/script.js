439802fff176708ff3cc1d5b7ca83162a6a9f6d6
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-07 23:17:19 +0100

    Adding Js

---
+++ 12-Load State/script.js
@@ -0,0 +1,9 @@
+const cardImage = document.querySelector('.card_image');
+const cardTitle = document.querySelector('.card_title');
+const cardDescription = document.querySelector('.card_description');
+
+console.log(CardTitle);
+const renderCard = () => {
+    cardTitle.textContent = 'Here goes the Card Title';
+    cardDescription.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quia suscipit placeat quod pariatur. Aliquid iste totam sed inventore veniam! Perspiciatis et voluptate modi culpa qui, vero autem obcaecati fugiat?';
+}
