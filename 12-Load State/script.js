b9608b487b1a38d453ed887e9afe86ea05c17c35
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-07 23:49:43 +0100

    Added Randomly generated image

--- 12-Load State/script.js
+++ 12-Load State/script.js
@@ -2,7 +2,6 @@ const cardImage = document.querySelector('.card_image');
 const cardTitle = document.querySelector('.card_title');
 const cardDescription = document.querySelector('.card_description');
 
-console.log(CardTitle);
 const renderCard = () => {
     cardTitle.textContent = 'Here goes the Card Title';
     cardDescription.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quia suscipit placeat quod pariatur. Aliquid iste totam sed inventore veniam! Perspiciatis et voluptate modi culpa qui, vero autem obcaecati fugiat?';
@@ -14,8 +13,12 @@ const renderCard = () => {
 
 function createImage() {
     const img = new Image();
+    img.classList.add("image");
+    img.setAttribute('alt', 'A randomly generated image');
+    img.setAttribute('src', 'https://source.unsplash.com/random/320x200');
+    cardImage.appendChild(img);
 
     console.log(img);
-}
+};
 
 createImage();
